7776472659d0f8fe79f78b5eb9a75c62bfdca02e
PraveenKumarpillai <praveenmca@outlook.com>
2023-06-02 19:56:08 +0530

    Parent-Child as String and Model ppassing muliple val

--- datapasscomp-app/src/app/parentinput/parentinput.component.ts
+++ datapasscomp-app/src/app/parentinput/parentinput.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./parentinput.component.css']
 })
 export class ParentinputComponent {
-
+  mystring="";
+  myobj:{"Name":string,"Country":string}={"Name":"","Country":""};
+  myProp={"MyString":"","Myobject":{"Name":"","Country":""}};
+  UpdatetoChild(t1:any,t2:any,t3:any)
+  {
+      this.mystring=t1;
+      this.myobj={"Name":t2,"Country":t3};
+      this.myProp.MyString=t1;
+      this.myProp.Myobject=this.myobj;
+  }
 }
